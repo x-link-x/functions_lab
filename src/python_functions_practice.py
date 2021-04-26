3f70699c4966872f90d59fd8420e93d28ad8af41
Link <xlinkx_@outlook.com>
2021-04-26 12:47:19 +0100

    made the tests pass

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -1,7 +1,40 @@
-# Write the code here
-
 def return_10():
     return 10
 
-def add(x, y):
-    return x + y
+def add(num_1, num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(string_1, string_2):
+    return int(string_1) + int(string_2)
+
+def number_to_full_month_name(number):
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    return months[number-1]
+
+def number_to_short_month_name(number):
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    return months[number-1][:3]
+
+def volume_of_cube(side_length):
+    return side_length**3
+
+def reverse_string(string):
+    return string[::-1]
+
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) / 1.8
